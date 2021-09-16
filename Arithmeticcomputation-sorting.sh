ed84c459311d9977250b51518d52b85a34d1d2b6
RJ1611 <rajasdhake1611@gmail.com>
2021-09-16 14:07:47 +0530

    three inputs program

---
+++ Arithmeticcomputation-sorting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Welcome to Arithmetic Computation & Sorting Computation"
+
+echo -n "Enter 1st number"
+read firstInput
+echo -n "Enter 2nd number"
+read secondInput
+echo -n "Enter 3rd number"
+read thirdInput
+
